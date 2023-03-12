63dd3446dcfc86f318168d1c49c22e2ae17f2cd2
Daniel Uribe <duribe@tevora.com>
2023-03-12 17:50:19 -0500

    allow all origins

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.enableCors();
+  app.enableCors({ origin: '*' });
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   const port = process.env.PORT || 3000;
   await app.listen(port, () => {
